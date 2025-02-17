b8725af751884750ae82bc505f5c444139d19f49
VarunKalathiya06 <164240233+VarunKalathiya06@users.noreply.github.com>
2025-02-16 22:03:04 -0800

    Admin panel support and Django shell commands added

--- LoginSystem/LoginSystem/settings.py
+++ LoginSystem/LoginSystem/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'LoginSystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'Loginify/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
